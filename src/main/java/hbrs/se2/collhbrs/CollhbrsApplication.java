43325e0b4407cc2d8756e1e0f4a7d6816d561c51
Russel Marcelo <russelmarcelo1509@gmail.com>
2024-05-19 17:38:14 +0200

    initial commit

---
+++ src/main/java/hbrs/se2/collhbrs/CollhbrsApplication.java
@@ -0,0 +1,13 @@
+package hbrs.se2.collhbrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollhbrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollhbrsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hbrs/se2/collhbrs/CollhbrsApplicationTests.java
@@ -0,0 +1,13 @@
+package hbrs.se2.collhbrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollhbrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
