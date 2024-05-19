c3cbdc571ff33f2cb4b53084e6ebe3733de9b82a
Russel Marcelo <russelmarcelo1509@gmail.com>
2024-05-19 20:57:38 +0200

    improved registration view

--- src/main/java/hbrs/se2/collhbrs/views/RegistrationView.java
+++ src/main/java/hbrs/se2/collhbrs/views/RegistrationView.java
@@ -1,41 +1,86 @@
 package hbrs.se2.collhbrs.views;
 
 import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.formlayout.FormLayout;
-import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.HasValueAndElement;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import hbrs.se2.collhbrs.entity.Benutzer;
 import hbrs.se2.collhbrs.entity.Profil;
 import hbrs.se2.collhbrs.service.RegisterService;
 
-@Route(value = "tmp")
-@PageTitle("User Registration")
-@CssImport("./styles/index.css")
-public class RegistrationView extends Div {
+import java.util.stream.Stream;
 
-    private TextField username = new TextField("Benutzername");
-    private PasswordField password = new PasswordField("Passwort");
-    private PasswordField passwordConfirmation = new PasswordField("Passwort (Wdh)");
-    private Button register = new Button("Registrieren");
+@Route(value = "registration")
+@CssImport("./styles/index.css")
+public class RegistrationView extends FormLayout {
+
+    private H3 title;
+    private TextField firstName;
+    private TextField lastName;
+    private TextField username;
+    private EmailField email;
+    private PasswordField password;
+    private PasswordField passwordConfirm;
+    private Span errorMessageField;
+    private Button submitButton;
 
     public RegistrationView(RegisterService registerService) {
 
         addClassName("register");
-        add(createTitle());
-        add(createFormLayout());
-        add(createButtonLayout());
-        clearForm();
+        title = new H3("Sign Up");
+        firstName = new TextField("First name");
+        lastName = new TextField("Last name");
+        username =  new TextField("Username");
+        email = new EmailField("Email");
+        password = new PasswordField("Password");
+        passwordConfirm = new PasswordField("Confirm password");
 
-        register.addClickListener(e -> {
+        setRequiredIndicatorVisible(
+                firstName,
+                lastName,
+                username,
+                email,
+                password,
+                passwordConfirm
+        );
+
+        errorMessageField = new Span();
+
+        submitButton = new Button("Sign Up");
+        submitButton.addThemeVariants(ButtonVariant.LUMO_ICON);
+        submitButton.addClassName("button-layout");
+
+        username.setWidth("100");
+
+        add(
+                title, firstName, lastName, username, email, password,
+                passwordConfirm, errorMessageField,
+                submitButton
+        );
+
+        setMaxWidth("500px");
+
+        setResponsiveSteps(
+                new ResponsiveStep("0", 1, ResponsiveStep.LabelsPosition.TOP),
+                new ResponsiveStep("490px", 2, ResponsiveStep.LabelsPosition.TOP)
+        );
+
+        setColspan(title, 2);
+        setColspan(email, 2);
+        setColspan(username, 2);
+        setColspan(errorMessageField, 2);
+        setColspan(submitButton, 2);
+
+        submitButton.addClickListener(e -> {
 
             Profil profil = new Profil();
             Benutzer benutzer = new Benutzer();
@@ -51,26 +96,15 @@ public class RegistrationView extends Div {
         });
     }
 
-    private void clearForm() {
-        password.clear();
-        passwordConfirmation.clear();
-    }
+    public PasswordField getPasswordField() { return password; }
 
-    private H3 createTitle() {
-        return new H3("Benutzer-Registration");
-    }
+    public PasswordField getPasswordConfirmField() { return passwordConfirm; }
 
-    private FormLayout createFormLayout() {
-        FormLayout formLayout = new FormLayout();
-        formLayout.add(username, password, passwordConfirmation);
-        return formLayout;
-    }
+    public Span getErrorMessageField() { return errorMessageField; }
 
-    private HorizontalLayout createButtonLayout() {
-        HorizontalLayout buttonLayout = new HorizontalLayout();
-        buttonLayout.addClassName("button-layout");
-        register.addThemeVariants(ButtonVariant.LUMO_CONTRAST);
-        buttonLayout.add(register);
-        return buttonLayout;
+    public Button getSubmitButton() { return submitButton; }
+
+    private void setRequiredIndicatorVisible(HasValueAndElement<?, ?>... components) {
+        Stream.of(components).forEach(comp -> comp.setRequiredIndicatorVisible(true));
     }
 }
