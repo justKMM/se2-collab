94803a3c020e2ed9fc9f26530d8993b92dc88cee
Russel Marcelo <russelmarcelo1509@gmail.com>
2024-05-20 17:02:39 +0200

    fix merge conflict

--- src/main/java/hbrs/se2/collhbrs/repository/ProfilRepository.java
+++ src/main/java/hbrs/se2/collhbrs/repository/ProfilRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProfilRepository extends JpaRepository<Profil, Long> {
-}
+public interface ProfilRepository extends JpaRepository<Profil, Long> { }
