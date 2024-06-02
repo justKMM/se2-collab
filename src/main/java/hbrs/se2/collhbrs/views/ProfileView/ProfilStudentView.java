7b6ad5cced371f17eb3f114a54bd62ddf8813530
Leon Kraschewski <leon.kraschewski@smail.inf.h-brs.de>
2024-06-03 00:32:38 +0200

    - refactored ProfilStudentLayout - refactored ProfilStudentView - change to database can be added

--- src/main/java/hbrs/se2/collhbrs/views/ProfileView/ProfilStudentView.java
+++ src/main/java/hbrs/se2/collhbrs/views/ProfileView/ProfilStudentView.java
@@ -11,9 +11,12 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H6;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
 import hbrs.se2.collhbrs.model.dto.StudentDTO;
@@ -32,75 +35,184 @@ import java.util.List;
 @CssImport("./styles/index.css")
 public class ProfilStudentView extends Composite<VerticalLayout> {
 
-    private final Button button_p_data_edit;
-    private final Button button_mail_edit;
-    private final Button button_merkzettel;
-    private final Button button_lebenslauf;
-    private final ProfilStudentLayout profileLayout;
-    private final Button button_cancel = new Button("Abbrechen");
-    private final Button button_confirm = new Button("Speichern");
+    private  Button button_p_data_edit;
+    private  Button button_mail_edit;
+    private  Button button_merkzettel;
+    private  Button button_lebenslauf;
+    private final ProfilStudentLayout profileLayout = new ProfilStudentLayout();
+    private final Button button_cancel = new Button("Cancel");
+    private final Button button_confirm = new Button("Save");
+
+
+    private VerticalLayout layoutColumn2 = new VerticalLayout();
+    private VerticalLayout layoutColumn3 = new VerticalLayout();
+    private VerticalLayout layoutColumn4 = new VerticalLayout();
+    private VerticalLayout layoutColumn5 = new VerticalLayout();
+
+    private HorizontalLayout layoutRow = new HorizontalLayout();
+    private HorizontalLayout layoutRow2 = new HorizontalLayout();
+    private HorizontalLayout layoutRow3 = new HorizontalLayout();
+
+    private H1 h12 = new H1("Personal data:");
+    private H1 h13 = new H1("Documents");
+    private H6 h6 = new H6("Rating:");
+    private H1 h1;
+
+    private Paragraph textMedium2 = new Paragraph("Lebenslauf:");
+
     Dialog dialog;
-    Icon icon;
+    private Icon icon = new Icon();
+    private Icon icon2 = new Icon();
+    private Icon icon3 = new Icon();
+    private Icon icon4 = new Icon();
+    private Icon icon5 = new Icon();
+
+    Avatar avatar = new Avatar();
 
     private StudentDTO student;
-    private List<Interest> interestList;
-    private List<FirstName> firstNameList;
-    private List<DegreeProgramm> degreeProgrammList;
-    private List<Skill> skillList;
 
+    private ProfileService profileService;
 
     @Autowired
     public ProfilStudentView(ProfileService profileService, SessionService sessionService) {
 
         student = sessionService.getCurrentStudent();
+        h1 = new H1("Hallo " + student.getUser().getUsername() +  "!");
 
-        H1 h1 = new H1("Hallo " + student.getUser().getUsername() +  "!");
-
-        VerticalLayout layoutColumn2 = new VerticalLayout();
-        HorizontalLayout layoutRow = new HorizontalLayout();
-        Avatar avatar = new Avatar();
-        VerticalLayout layoutColumn3 = new VerticalLayout();
-
-        H6 h6 = new H6("Bewertung:");
-        HorizontalLayout layoutRow2 = new HorizontalLayout();
-
-        // müssen noch ausgewählt werden für das Rating (später)
-        icon = new Icon();
-        Icon icon2 = new Icon();
-        Icon icon3 = new Icon();
-        Icon icon4 = new Icon();
-        Icon icon5 = new Icon();
 
         // dialog Object für Änderungsmaske
         dialog = new Dialog();
-        button_mail_edit = new Button("Email bearbeiten");
-        button_merkzettel = new Button("Merkzettel");
-        button_lebenslauf = new Button("Lebenslauf hochladen");
-        button_p_data_edit = new Button("Persönliche Daten bearbeiten");
 
-        HorizontalLayout layoutRow3 = new HorizontalLayout();
-        VerticalLayout layoutColumn4 = new VerticalLayout();
-        H1 h12 = new H1("Persönliche Daten:");
 
-        H1 h13 = new H1("Dokumente");
-        Paragraph textMedium2 = new Paragraph("Lebenslauf:");
+
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        getContent().setFlexGrow(1.0, layoutColumn2);
+
+        setRatingIcons();
+        setLayouts();
+        setButtons();
+
+
+        // dialog = getContentProfile();
+
+        button_p_data_edit.addClickListener(e -> {
+            dialog.setHeaderTitle("Change personal data");
+            dialog.add(profileLayout);
+            createFooter(dialog);
+            dialog.open();
+        });
+
+        button_mail_edit.addClickListener(e -> {
+            Dialog d = new Dialog();
+            VerticalLayout verticalLayout = new VerticalLayout();
+            EmailField ef_email = new EmailField("E-Mail:");
+            Button button_confirm_mail = new Button("Save");
+            Button button_cancel_mail = new Button("Cancel");
+            verticalLayout.add(ef_email);
+            verticalLayout.getStyle().set("width", "20rem").set("max-width", "100%");
+            d.setHeaderTitle("E-Mail ändern: ");
+            d.add(verticalLayout);
+            d.getFooter().add(button_cancel_mail, button_confirm_mail);
+
+            button_cancel_mail.addClickListener(e1 -> d.close());
+            button_confirm_mail.addClickListener(e1 -> {
+                if(!ef_email.isEmpty()) {
+                    student.getUser().setEmail(ef_email.getValue().toString());
+                    Notification.show("E-Mail wurde geändert. ");
+                }
+                dialog.close();
+            });
+
+            d.open();
+        });
+
+
+
+
+
+        String s = "Email: " + sessionService.getCurrentStudent().getUser().getEmail() + "\n" +
+                " Last name: " + sessionService.getCurrentStudent().getLastName() + "\n" +
+                " Username: " +  sessionService.getCurrentStudent().getUser().getUsername();
+        Paragraph textMedium = new Paragraph(
+               s);
+
+        textMedium.setWidth("100%");
+        textMedium.getStyle().set("font-size", "var(--lumo-font-size-m)");
+        layoutColumn4.add(h12, textMedium, button_p_data_edit);
+
+
+
+
+    }
+
+    private void setButtons() {
+        button_mail_edit = new Button("Edit Email");
+        button_merkzettel = new Button("Notespad");
+        button_lebenslauf = new Button("upload Lebenslauf");
+        button_p_data_edit = new Button("Edit personal data");
 
         button_mail_edit.setWidth("min-content");
-        layoutColumn3.setAlignSelf(FlexComponent.Alignment.START, button_merkzettel);
         button_merkzettel.setWidth("min-content");
         button_merkzettel.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         button_lebenslauf.setWidth("min-content");
         button_lebenslauf.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         button_p_data_edit.setWidth("min-content");
         button_p_data_edit.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+    }
 
-        //WTF????
-        // Bullshit, braucht man aber fürs Design
-        VerticalLayout layoutColumn5 = new VerticalLayout();
-        getContent().setWidth("100%");
-        getContent().getStyle().set("flex-grow", "1");
+    public void createFooter(Dialog d) {
+
+        button_confirm.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        button_cancel.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        button_confirm.addClickShortcut(Key.ENTER);
+        button_cancel.addClickShortcut(Key.ESCAPE);
+
+        button_cancel.addClickListener(e -> d.close());
+        button_confirm.addClickListener(e -> {
+            changeData();
+
+
+
+            dialog.close();
+        });
+        // speichern fehlt noch
+        d.getFooter().add(button_cancel);
+        d.getFooter().add(button_confirm);
+    }
+
+    private void setRatingIcons() {
+        // müssen noch ausgewählt werden für das Rating (später)
+
+        icon = VaadinIcon.STAR.create();
+        icon2 = VaadinIcon.STAR.create();
+        icon3 = VaadinIcon.STAR.create();
+        icon4 = VaadinIcon.STAR.create();
+        icon5 = VaadinIcon.STAR.create();
+    }
+
+    private void changeData(){
+        User u = student.getUser();
+        u.setUsername(profileLayout.getTf_vorname());
+
+        if(!profileLayout.getTf_vorname().isEmpty()) {
+            student.setUser(u);
+            // Notification.show("First name has been changed.");
+        }
+        if(!profileLayout.getTf_nachname().isEmpty()) {
+            student.setLastName(profileLayout.getTf_nachname());
+            // Notification.show("Last name has been changed.  ");
+
+
+        }
+
+
+
+    }
+
+    private void setLayouts(){
+        layoutColumn3.setAlignSelf(FlexComponent.Alignment.START, button_merkzettel);
         layoutColumn2.setWidthFull();
-        getContent().setFlexGrow(1.0, layoutColumn2);
         layoutColumn2.setWidth("100%");
         layoutColumn2.getStyle().set("flex-grow", "1");
         layoutRow.setWidthFull();
@@ -147,6 +259,8 @@ public class ProfilStudentView extends Composite<VerticalLayout> {
         textMedium2.getStyle().set("font-size", "var(--lumo-font-size-m)");
 
 
+        profileLayout.setWidth("50em");
+
         // Objekte den Layouts zuweisen
         getContent().add(layoutColumn2);
         layoutRow.add(avatar, layoutColumn3);
@@ -156,116 +270,6 @@ public class ProfilStudentView extends Composite<VerticalLayout> {
         layoutColumn3.add(h1, h6, layoutRow2, button_mail_edit, button_merkzettel);
 
         layoutColumn5.add(h13, textMedium2, button_lebenslauf);
-
-
-        //setRatingIcons();
-
-        // dialog = getContentProfile();
-
-        button_p_data_edit.addClickListener(e -> dialog.open());
-
-
-        // Aus der Methode rausgezogen um Zugriff auf ProfileLayout zu haben
-
-        profileLayout = new ProfilStudentLayout();
-
-        profileLayout.setWidth("50em");
-
-        dialog.setHeaderTitle("Persönliche Daten ändern");
-        dialog.add(profileLayout);
-
-        createFooter(dialog);
-
-        Paragraph textMedium = new Paragraph(
-                "Email: " + sessionService.getCurrentStudent().getUser().getEmail() +
-                        "Nachname: " + sessionService.getCurrentStudent().getLastName() +
-                        "Username: " +  sessionService.getCurrentStudent().getUser().getUsername());
-
-        textMedium.setWidth("100%");
-        textMedium.getStyle().set("font-size", "var(--lumo-font-size-m)");
-        layoutColumn4.add(h12, textMedium, button_p_data_edit);
-
-
-        //Utils? oder Service?
-        button_confirm.addClickListener(e -> {
-
-            String lastName = profileLayout.getTf_nachname().getValue();
-            student.setLastName(lastName);
-
-            //TODO: Factory!
-            String[] interests = profileLayout.getInterestField().getValue().split(" ");
-            interestList = new ArrayList<>();
-            for (String interestName : interests) {
-                Interest interest = new Interest();
-                interest.setInterestName(interestName);
-                interest.setStudent(student.getStudent());
-                interestList.add(interest);
-            }
-            String[] firstNames = profileLayout.getTf_vorname().getValue().split(" ");
-            firstNameList = new ArrayList<>();
-            for (String firstNameName : firstNames) {
-                FirstName firstName = new FirstName();
-                firstName.setFirstNameName(firstNameName);
-                firstName.setStudent(student.getStudent());
-                firstNameList.add(firstName);
-            }
-
-            String[] skills = profileLayout.getSkillsField().getValue().split(" ");
-            skillList = new ArrayList<>();
-            for (String skillName : skills) {
-                Skill skill = new Skill();
-                skill.setSkillName(skillName);
-                skill.setStudent(student.getStudent());
-                skillList.add(skill);
-            }
-            // Studiengänge ziehen und setzen
-            String[] degrees = profileLayout.getDegreeField().getValue().split("");
-            degreeProgrammList = new ArrayList<>();
-            for (String degreeProgrammName : degrees) {
-                DegreeProgramm degreeProgramm = new DegreeProgramm();
-                degreeProgramm.setDegreeProgrammName(degreeProgrammName);
-                degreeProgramm.setStudent(student.getStudent());
-                degreeProgrammList.add(degreeProgramm);
-            }
-
-            student.getUser().getProfile().setXingUsername(profileLayout.getXingUsernameField().getValue());
-            student.getUser().getProfile().setLinkedinUsername(profileLayout.getLinkedinUsernameField().getValue());
-            student.getUser().getProfile().setProfileDescription(profileLayout.getProfileDescriptionField().getValue());
-            student.getUser().getProfile().setAvatarUrl(profileLayout.getAvatarUrlField().getValue());
-
-            profileService.saveProfile(student.getUser().getProfile());
-
-            for (Interest interest : interestList) {
-                profileService.saveInterest(interest);
-            }
-            for (FirstName firstName : firstNameList) {
-                profileService.saveFirstName(firstName);
-            }
-            for (Skill skill : skillList) {
-                profileService.saveSkill(skill);
-            }
-            for (DegreeProgramm degreeProgramm : degreeProgrammList) {
-                profileService.saveDegreeProgramm(degreeProgramm);
-            }
-            dialog.close();
-        });
-
-
-    }
-    public void createFooter(Dialog d) {
-
-        button_confirm.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        button_cancel.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        button_confirm.addClickShortcut(Key.ENTER);
-        button_cancel.addClickShortcut(Key.ESCAPE);
-
-        button_cancel.addClickListener(e -> d.close());
-        // speichern fehlt noch
-        d.getFooter().add(button_cancel);
-        d.getFooter().add(button_confirm);
     }
 
-    public void setRatingIcons() {
-        icon = new Icon("vaadin:star");
-    }
 }
