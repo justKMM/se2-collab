d24b399f5cfe38f5f6177b171a3bd9bf77f0c142
Luca Ringhausen <luca.ringhausen@h-brs.de>
2024-06-26 11:24:49 +0200

    fixed main class

--- src/main/java/hbrs/se2/collhbrs/CollhbrsApplication.java
+++ src/main/java/hbrs/se2/collhbrs/CollhbrsApplication.java
@@ -2,9 +2,10 @@ package hbrs.se2.collhbrs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CollhbrsApplication {
+public class CollhbrsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CollhbrsApplication.class, args);
