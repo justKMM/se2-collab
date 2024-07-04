09a129be1008e91fe57182310dc733f8aa908c72
Leon Kraschewski <leon.kraschewski@smail.inf.h-brs.de>
2024-07-04 16:39:54 +0200

    added ApplyView

---
+++ src/main/java/hbrs/se2/collhbrs/views/profile/student/ApplyView.java
@@ -0,0 +1,19 @@
+package hbrs.se2.collhbrs.views.profile.student;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import hbrs.se2.collhbrs.util.Globals;
+import hbrs.se2.collhbrs.views.AppView;
+import jakarta.annotation.security.RolesAllowed;
+
+
+@CssImport("./styles/index.css")
+@Route(value = Globals.Pages.APPlY_STUDENT, layout = AppView.class)
+@RolesAllowed(Globals.Roles.STUDENT)
+public class ApplyView extends Composite<VerticalLayout> {
+
+
+
+}
