986786ef46c19a18b9027f287cc90a52dd8af70a
ThomasBroese <110627227+ThomasBroese@users.noreply.github.com>
2024-07-08 10:55:22 +0200

    changed business entity

--- src/main/java/hbrs/se2/collhbrs/model/entity/Business.java
+++ src/main/java/hbrs/se2/collhbrs/model/entity/Business.java
@@ -26,19 +26,41 @@ public class Business implements Serializable {
     @Column(name = "name", length = 128, nullable = false)
     private String name;
 
+    @Basic
+    @Column(name = "adresse", length = 128)
+    private String address;
+
+    @Basic
+    @Column(name = "stadt", length = 128)
+    private String city;
+
+    @Basic
+    @Column(name = "postleitzahl", length = 8)
+    private String zipCode;
+
+    @Basic
+    @Column(name = "land", length = 128)
+    private String country;
+
+
+
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Business that = (Business) o;
-        return businessID == that.businessID &&
-                Objects.equals(user, that.user) &&
-                Objects.equals(name, that.name);
+        Business business = (Business) o;
+        return businessID == business.businessID &&
+                Objects.equals(user, business.user) &&
+                Objects.equals(name, business.name) &&
+                Objects.equals(address, business.address) &&
+                Objects.equals(city, business.city) &&
+                Objects.equals(zipCode, business.zipCode) &&
+                Objects.equals(country, business.country);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(businessID, user, name);
+        return Objects.hash(businessID, user, name, address, city, zipCode, country);
     }
 }
